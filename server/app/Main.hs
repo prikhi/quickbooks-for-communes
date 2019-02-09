9445adf9a45786a6ba5b6dd26129b0c1b6257ed5
Pavan Rikhi <pavan.rikhi@gmail.com>
2019-02-09 03:23:52 -0500

    Fix Warnings & Formatting

--- server/app/Main.hs
+++ server/app/Main.hs
@@ -1,18 +1,33 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Main where
 
-import App (app)
-import Network.Wai (requestBody)
-import Network.Wai.Handler.Warp (setPort, setHost, defaultSettings)
-import Network.Wai.Handler.WarpTLS (runTLS, tlsSettings)
+import           App                            ( app )
+import           Network.Wai                    ( Application )
+import           Network.Wai.Internal           ( Request
+                                                , Response
+                                                , ResponseReceived
+                                                , getRequestBodyChunk
+                                                )
+import           Network.Wai.Handler.Warp       ( setPort
+                                                , setHost
+                                                , defaultSettings
+                                                )
+import           Network.Wai.Handler.WarpTLS    ( runTLS
+                                                , tlsSettings
+                                                )
 
 main :: IO ()
-main = runTLS
-    (tlsSettings "cert.pem" "key.pem")
-    (setHost "*" $ setPort 3000 $ defaultSettings)
-    $ logAllMiddleware app
+main =
+    runTLS (tlsSettings "cert.pem" "key.pem")
+           (setHost "*" $ setPort 3000 $ defaultSettings)
+        $ logAllMiddleware app
 
+logAllMiddleware
+    :: Application
+    -> Request
+    -> (Response -> IO ResponseReceived)
+    -> IO ResponseReceived
 logAllMiddleware app_ req respond = do
     print req
-    print =<< requestBody req
+    print =<< getRequestBodyChunk req
     app_ req respond
