222f4b2fc3cffcd0b65e4554c828ad4492dfb9b6
Pavan Rikhi <pavan.rikhi@gmail.com>
2019-02-14 02:22:54 -0500

    Use WAI Request Logger Instead of Custom Middlware
    The request logger properly refills the consumed request body, our
    middleware did not do this & caused body parsing issues.

--- server/app/Main.hs
+++ server/app/Main.hs
@@ -2,12 +2,6 @@
 module Main where
 
 import           App                            ( app )
-import           Network.Wai                    ( Application )
-import           Network.Wai.Internal           ( Request
-                                                , Response
-                                                , ResponseReceived
-                                                , getRequestBodyChunk
-                                                )
 import           Network.Wai.Handler.Warp       ( setPort
                                                 , setHost
                                                 , defaultSettings
@@ -15,19 +9,11 @@ import           Network.Wai.Handler.Warp       ( setPort
 import           Network.Wai.Handler.WarpTLS    ( runTLS
                                                 , tlsSettings
                                                 )
+import           Network.Wai.Middleware.RequestLogger
+                                                ( logStdoutDev )
 
 main :: IO ()
 main =
     runTLS (tlsSettings "cert.pem" "key.pem")
            (setHost "*" $ setPort 3000 $ defaultSettings)
-        $ logAllMiddleware app
-
-logAllMiddleware
-    :: Application
-    -> Request
-    -> (Response -> IO ResponseReceived)
-    -> IO ResponseReceived
-logAllMiddleware app_ req respond = do
-    print req
-    print =<< getRequestBodyChunk req
-    app_ req respond
+        $ logStdoutDev app
