4a0ee9825afe908e912abf4afc2bc873a8e3cf94
Pavan Rikhi <pavan.rikhi@gmail.com>
2019-04-02 12:29:13 -0400

    Update Stack LTS

--- manage.hs
+++ manage.hs
@@ -7,7 +7,7 @@
     --ghc-options -Wincomplete-record-updates
     --ghc-options -Wincomplete-uni-patterns
     --ghc-options -Wredundant-constraints
-    --resolver lts-13.14
+    --resolver lts-13.15
     --package ansi-terminal
     --package async
     --package directory
